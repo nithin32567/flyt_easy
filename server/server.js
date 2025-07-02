50d80186872cd067fba2172c2489c1ea132c9928
Nithin prathapan <nithinprathapan32567@gmail.com>
2025-07-02 23:32:40 +0530

    path added

--- server/server.js
+++ server/server.js
@@ -24,6 +24,13 @@ app.use(
   })
 );
 
+
+app.use(express.static(path.resolve(__dirname, '../client','dist')))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+});
+
 app.use("/api", indexRoutes);
 app.use("/api/hotel", hotelRoutes);
 app.use("/api", razorpayRoutes);
