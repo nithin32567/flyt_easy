10e6e99ba9f6fce0e80b4777d6f48913d5622822
nithinprathapan <nithinprathapan32567@gmail.com>
2025-07-28 20:48:25 +0530

    ui completed

--- client/src/pages/FlightSearch.jsx
+++ client/src/pages/FlightSearch.jsx
@@ -21,7 +21,9 @@ const FlightSearch = () => {
         <Demobanner />
       </div>
 
-      <div className="relative z-20 w-full flex flex-col gap-4 px-4 py-8 md:max-w-7xl mx-auto ">
+      <div className="
+      -translate-y-[5%]
+      relative z-20 w-full flex flex-col gap-4 px-4 py-8 md:max-w-7xl mx-auto ">
         <SearchForm />
         <div className="w-full max-w-7xl mx-auto">
           <div className="w-full flex flex-col sm:flex-row justify-between items-center">
